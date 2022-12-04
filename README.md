64f827bebbc5f1dd27fcedcd265074abbc5bcfcf
uraphero <119469745+uraphero@users.noreply.github.com>
2022-12-04 11:40:24 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# chartjs-sample-1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-5baemo)
---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import { Chart } from 'chart.js/auto';
+
+const ctx = document.querySelector<HTMLCanvasElement>('#chart');
+
+if (ctx) {
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [
+        {
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+}
+
+export {};
